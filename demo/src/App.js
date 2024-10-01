762423c1064dfcf2794abc7a47b4e541b020c579
karthik <harthikkpandya@gmail.com>
2024-10-01 20:44:42 +0530

    initial commit

---
+++ demo/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Head from './components/mainpages.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Head/>
+  
+    </div>
+  );
+}
+
+export default App;
